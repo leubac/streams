7fc0b9c80d4701ab405877a5e296e0c5fc789ed3
Cyril Leuba <c.leuba@panipro.ch>
2018-06-19 07:34:02 +0200

    adding streams class

---
+++ Streams/Streams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Streams
+{
+    public sealed class Streams : IEnumerable<Stream>, IDisposable
+    {
+        private readonly IEnumerable<Stream> streams;
+        public Streams(IEnumerable<Stream> streams) => this.streams = streams;
+        public void Dispose()
+        {
+            foreach (var stream in streams)
+            {
+                stream.Dispose();
+            }
+        }
+        public IEnumerator<Stream> GetEnumerator() => streams.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
